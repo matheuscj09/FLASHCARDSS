d2bd789bb0a3951f0ea1dabfd3d24f098e0e0365
matheuscj09 <matheus.ruckel@escola.pr.gov.br>
2025-05-26 19:59:33 -0300

    Add files via upload

---
+++ flashcard/perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Programação',
+    'O que significa a lógica de programação e por que ela é importante',
+    'Lógica de programação é o raciocínio usado para resolver problemas por meio de códigos. Ela é importante porque permite criar soluções eficientes e funcionais, organizando os passos que o programa deve seguir.'
+)
+
+criaCartao(
+    'Geografia',
+    'Sabe qual é a maior montanha do mundo?',
+    'A maior montanha do mundo é o Monte Everest.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um bug na programação?',
+    'É um erro ou falha no código que faz com que o programa não funcione como deveria.'
+)
+
+criaCartao(
+    'Química',
+    'O que acontece quando misturamos vinagre e bicarbonato de sódio?',
+    'Ocorre uma reação química que libera dióxido de carbono (gás), formando bolhas e espuma.'
+)
+
+criaCartao(
+    'história',
+    'Sabe em que ano o Brasil foi descoberto?',
+    'O Brasil foi descoberto no ano de 1500 por Pedro Alvares Cabral.'
+)
+
+criaCartao(
+    'Física',
+    'O que é mais rápido: a luz ou o som? Por quê?',
+    'A luz, porque ela se propaga a cerca de 300.000 km/s no vácuo, enquanto o som viaja a aproximadamente 340 m/s no ar.'
+)
+
+criaCartao(
+    'Matemática',
+    'Por que Pitágoras ficou famoso na matemática?'
+    'Porque desenvolveu o Teorema de Pitágoras, que relaciona os lados de um triângulo retângulo, e também fundou uma escola que misturava matemática, filosofia e espiritualidade.'
+)
+
+criaCartao(
+    'Física',
+    'Por que os astronautas flutuam na Estação Espacial Internacional?',
+    'Porque estão em queda livre constante ao redor da Terra, experimentando microgravidade.'
+)
+
+criaCartao(
+    'Matemática',
+    'Quem foi Bhaskara?',
+    'Bhaskara foi um matemático e astrônomo indiano do século XII, conhecido por desenvolver uma fórmula para resolver equações quadráticas, chamada de Fórmula de Bhaskara.'
+)
